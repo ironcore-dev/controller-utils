2bb37dd2687397935db06cd3dc76be60e7bc0483
Lim, Kian-An <lim.kian.an@gmx.de>
2021-12-22 11:43:33 +0100

    Add a func to generate a slice of CRD pointers from a slice of CRDs (#34)
    * Add a func to generate a slice of CRD pointers from a slice of CRDs
    
    Signed-off-by: Lim, Kian-An <lim.kian.an@gmx.de>
    
    * Update go.mod
    
    Co-authored-by: Andreas Fritzler <andreas.fritzler@sap.com>
    
    * go mod tidy
    
    Co-authored-by: Andreas Fritzler <andreas.fritzler@sap.com>

---
+++ envtestutils/envtestutils.go
@@ -0,0 +1,25 @@
+// Copyright 2021 OnMetal authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package envtestutils
+
+import apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+
+// CRDPtrsFromCRDs generates a slice of CRD pointers from a slice of CRDs
+func CRDPtrsFromCRDs(crds []apiextensionsv1.CustomResourceDefinition) (ptrs []*apiextensionsv1.CustomResourceDefinition) {
+	for i := range crds {
+		ptrs = append(ptrs, &crds[i])
+	}
+	return
+}
---
+++ envtestutils/envtestutils_test.go
@@ -0,0 +1,32 @@
+// Copyright 2021 OnMetal authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package envtestutils
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+)
+
+var _ = Describe("CRDPtrsFromCRDs", func() {
+	It("returns CRD pointers from CRDs", func() {
+		crdA := apiextensionsv1.CustomResourceDefinition{}
+		crdB := apiextensionsv1.CustomResourceDefinition{}
+
+		crds := []apiextensionsv1.CustomResourceDefinition{crdA, crdB}
+
+		Expect(CRDPtrsFromCRDs(crds)).To(Equal([]*apiextensionsv1.CustomResourceDefinition{&crdA, &crdB}))
+	})
+})
