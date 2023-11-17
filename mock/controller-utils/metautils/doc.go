7c64aa558826637f8eda0186649cd4f7ef9cda92
Andreas Fritzler <andreas.fritzler@sap.com>
2023-11-17 13:48:38 +0100

    Add reuse to project (#318)

--- mock/controller-utils/metautils/doc.go
+++ mock/controller-utils/metautils/doc.go
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: 2023 SAP SE or an SAP affiliate company and IronCore contributors
+// SPDX-License-Identifier: Apache-2.0
+
 //go:generate $MOCKGEN -copyright_file ../../../hack/boilerplate.go.txt -package metautils -destination=funcs.go github.com/ironcore-dev/controller-utils/mock/controller-utils/metautils EachListItemFunc
 package metautils
 
