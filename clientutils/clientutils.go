0e45f7936e6c84313554b5dd649d2499a6e648cb
Felix Riegger <felix.riegger@sap.com>
2026-09-08 14:20:39 +0200

    Remove year from license headers
    Signed-off-by: Felix Riegger <felix.riegger@sap.com>

--- clientutils/clientutils.go
+++ clientutils/clientutils.go
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2023 SAP SE or an SAP affiliate company and IronCore contributors
+// SPDX-FileCopyrightText: SAP SE or an SAP affiliate company and IronCore contributors
 // SPDX-License-Identifier: Apache-2.0
 
 // Package clientutils provides utilities for working with the client package of
