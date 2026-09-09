2d7ee16325084c75b5e4e0e4ddfa621397e4bc9f
Felix Riegger <felix.riegger@sap.com>
2026-09-09 09:49:25 +0200

    Remove year from license headers (#500)
    Signed-off-by: Felix Riegger <felix.riegger@sap.com>

--- mock/controller-utils/metautils/doc.go
+++ mock/controller-utils/metautils/doc.go
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2023 SAP SE or an SAP affiliate company and IronCore contributors
+// SPDX-FileCopyrightText: SAP SE or an SAP affiliate company and IronCore contributors
 // SPDX-License-Identifier: Apache-2.0
 
 //go:generate $MOCKGEN -copyright_file ../../../hack/boilerplate.go.txt -package metautils -destination=funcs.go github.com/ironcore-dev/controller-utils/mock/controller-utils/metautils EachListItemFunc
